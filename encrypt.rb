0ebd76f5a9a465a75c3f7d143910dbbb4f854e28
crew7 <tyjesp@gmail.com>
2023-05-31 22:00:31 -0400

    Completed make_downcased function

--- encrypt.rb
+++ encrypt.rb
@@ -1,7 +1,18 @@
 def encrypt(original_string, moved_characters)
     split_string = original_string.split("")
     cased_indexes = check_if_cased(split_string)
-    cased_indexes
+    downcased_split_string = make_downcased(split_string)
+    downcased_split_string
+end
+
+def make_downcased(split_string)
+    split_string.map do |character|
+        if isLetter(character)
+            character = character.downcase
+        else
+            character
+        end
+    end
 end
 
 
@@ -9,7 +20,7 @@ def check_if_cased(split_string)
     isCased = []
 
     split_string.each_with_index do |character,index|
-        if character.index(/[A-Za-z]/) != nil
+        if isLetter(character)
             if character != character.downcase
                 isCased.push(index)
             end
@@ -21,7 +32,9 @@ def check_if_cased(split_string)
 end
 
 
-
+def isLetter(character)
+    character.index(/[A-Za-z]/) != nil
+end
 
 
 
